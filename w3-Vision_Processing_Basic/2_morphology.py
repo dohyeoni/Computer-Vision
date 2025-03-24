a96b1efd0126dcc3a7050d37df754662a3fd5c62
dohyeoni <2dh18@naver.com>
2025-03-25 00:10:00 +0900

    Update 2_morphology.py

--- w3-Vision_Processing_Basic/2_morphology.py
+++ w3-Vision_Processing_Basic/2_morphology.py
@@ -17,7 +17,16 @@ b_erosion = cv.morphologyEx(image, cv.MORPH_ERODE, kernel)    # 침식
 b_open = cv.morphologyEx(image, cv.MORPH_OPEN, kernel)        # 열림
 b_close = cv.morphologyEx(image, cv.MORPH_CLOSE, kernel)      # 닫힘 
 
-result = np.hstack([image, b_dilation, b_erosion, b_open, b_close])
+images = [image, b_dilation, b_erosion, b_open, b_close]
+titles = ['Original','Dilation', 'Erosion', 'Open', 'Close']
 
-plt.imshow(result)
+# 서브플롯 생성 (1행 5열)
+fig, axes = plt.subplots(1, 5, figsize=(15, 5))
+
+for i, ax in enumerate(axes):
+    ax.imshow(images[i], cmap='gray')       # 이미지 표시
+    ax.set_title(titles[i], fontsize=12)    # 타이틀 표시
+    ax.axis('off')                          # 눈금 표시하지 않음
+
+plt.tight_layout()
 plt.show()

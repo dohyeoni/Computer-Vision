858d0fbbbd37bd063ceee8d30ea7124191f6bc36
dohyeoni <2dh18@naver.com>
2025-04-08 23:09:13 +0900

    Update 1_SIFT.py

--- w5-Local Feature/1_SIFT.py
+++ w5-Local Feature/1_SIFT.py
@@ -6,7 +6,7 @@ img = cv.imread('mot_color70.jpg')          # 이미지 불러오기
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 sift = cv.SIFT_create()      # SIFT 객체 생성성
-sift = cv.SIFT_create(nfeatures=100)  
+# sift = cv.SIFT_create(nfeatures=100)  
 kp, des = sift.detectAndCompute(gray, None)     # 특징점 검출
 
 gray = cv.drawKeypoints(gray, kp, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)    # 특징점을 이미지에 시각화
